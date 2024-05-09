5b0545d03be87d7a6bd20764667f63f74b346e9f
Dayana Torres <luzdayanatorres@gmail.com>
2024-05-09 17:14:52 -0500

    Funcionalidades 1.3.1

--- Uancinema_app/app/src/main/java/com/example/uancinema_app/VisualizacionActivity.kt
+++ Uancinema_app/app/src/main/java/com/example/uancinema_app/VisualizacionActivity.kt
@@ -12,4 +12,28 @@ class VisualizacionActivity: AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_visualizacion_peliculas)
     }
+    fun openDoctorStranger(view: View) {
+        val intent = Intent(this, PeliculaDoctorStranger::class.java).apply { }
+        startActivity(intent)
+    }
+    fun openMillers(view: View) {
+        val intent = Intent(this, PeliculaMillers::class.java).apply { }
+        startActivity(intent)
+    }
+    fun openAnabelle(view: View) {
+        val intent = Intent(this, PeliculaAnabelle::class.java).apply { }
+        startActivity(intent)
+    }
+    fun openRatatouille(view: View) {
+        val intent = Intent(this, PeliculaRatatouille::class.java).apply { }
+        startActivity(intent)
+    }
+    fun openMalefica(view: View) {
+        val intent = Intent(this, PeliculaMalefica::class.java).apply { }
+        startActivity(intent)
+    }
+    fun openAvatar(view: View) {
+        val intent = Intent(this, PeliculaAvatar::class.java).apply { }
+        startActivity(intent)
+    }
 }
